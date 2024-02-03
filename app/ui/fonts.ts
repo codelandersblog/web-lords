cca81b0146668bf50cd54a0bc008f92962052697
codelandersblog <info@codelanders.com>
2024-02-03 23:06:32 +0200

    Configure fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Lusitana, Permanent_Marker } from "next/font/google";
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+export const marker = Permanent_Marker({ weight: "400", subsets: ["latin"] });
