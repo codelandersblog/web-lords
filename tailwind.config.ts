1edfacbf02946199d2d9f132b09e5266c1d2e133
codelandersblog <info@codelanders.com>
2024-02-03 23:06:22 +0200

    Configure colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "primary-light": "#6750A4",
+        "on-primary-light": "#FFFFFF",
+        "primary-dark": "#D0BCFF",
+        "on-primary-dark": "#381E72",
       },
     },
   },
